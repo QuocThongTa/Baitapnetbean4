b6949bbef378aed8365000bbbaf73aa51198c464
777.thongyhoo@gmail.com <>
2024-08-28 09:50:55 +0700

    Responsible person:Thong

---
+++ src/exercise4/Exercise4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Exercise4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Nhap so luong phan tu mang: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        // TODO code application logic here
+    }
+    
+}
